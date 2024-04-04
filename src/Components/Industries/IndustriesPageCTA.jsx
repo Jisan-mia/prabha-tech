e6e9a135cde17216f8209efb2b62656f074fac22
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2024-04-04 19:47:49 +0600

    industries page call to action section

--- src/Components/Industries/IndustriesPageCTA.jsx
+++ src/Components/Industries/IndustriesPageCTA.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
-const IndustriesPageCTA = () => {
+import React from "react";
+import "./IndustriesPageCTA.css";
+const IndustriesPageCTA = ({ ctaText }) => {
   return (
-    <div>IndustriesPageCTA</div>
-  )
-}
+    <div className="industries__cta">
+      <div className="industries__cta-content container">
+        <div className="cta__wrapper">
+          <h2>{ctaText}</h2>
+          <button style={{zIndex:1}} className="primary-button">Connect with an Expert</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default IndustriesPageCTA
+export default IndustriesPageCTA;
--- src/Pages/Industries.jsx
+++ src/Pages/Industries.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import FeaturesList from "../Components/Industries/FeaturesList";
+import IndustriesPageCTA from "../Components/Industries/IndustriesPageCTA";
 import IndustriesPartner from "../Components/Industries/IndustriesPartner";
 import IndustriesWeServe from "../Components/Industries/IndustriesWeServe";
 
 const IndustriesPage = () => {
+  const ctaText = (
+    <>
+      <span className="text__gradient">Digitally transform your business’</span>
+      &nbsp;
+      <br /> with industry-best development services
+    </>
+  );
   return (
     <>
       <IndustriesPartner />
       <IndustriesWeServe />
       <FeaturesList />
+      <IndustriesPageCTA ctaText={ctaText} />
     </>
   );
 };
